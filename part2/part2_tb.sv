5092ec70065c96e7df37843da9344348d309018c
Vishal Goyal <goyal.vishal1205@gmail.com>
2019-09-22 02:40:09 -0400

    Moving arithmetic checks from test bench to a task, calling the task later on

--- part2/part2_tb.sv
+++ part2/part2_tb.sv
@@ -17,40 +17,12 @@ module tb_part2();
 
    // PLACE HOLDER FOR ALL TASK FILES
    `include "./tasks/reset_checks.sv"
-
-   int i;
-   int indata, outdata;
-   int ifh, ofh;
+   `include "./tasks/arith_checks.sv"
 
    initial begin
-   ifh=$fopen("./inputDataPart2", "r");
-   ofh=$fopen("./expectedOutputPart2", "r");
-
-      // Before first clock edge, initialize
-      reset = 1;
-      a = 0;
-      valid_in = 0;
-
-      @(posedge clk);
-      #1; // After 1 posedge
-      reset = 0; a = 10; valid_in = 0;
-      
-      for (i = 0; i < 100; i++) begin
-	      @(posedge clk);
-	      $fscanf(ifh,"%h\n", indata);
-	      $fscanf(ofh,"%h\n", outdata);
-	      #1;
-      	      assign valid_in = 1;
-	      assign a = indata[7:0];
-
-	      assert (f == outdata[19:0]) 
-	      	      $display("actual output %x and expected output %x match", f, outdata); 
-	      else
-		      $error("mismatch in actual output data %x and expected output data %x", f, outdata);
-      end
-
-      #20;
-      $finish;
+	arith_checks();
+	#20;
+	$finish;
    end // initial begin arith test
 
    logic [19:0] out_data_old, out_data_new;
@@ -59,7 +31,7 @@ module tb_part2();
 	   out_data_old <= (reset == 1) ? 'b0 : ((valid_out == 1) ? out_data_new : out_data_old);
    end
 
-   initial begin
+/*   initial begin
 	   @(posedge clk)
 	   assert property ((valid_out) |-> (out_data_old <= out_data_new))
 	   $display("%x old data, %x new data", $out_data_old, $out_data_new);
@@ -67,10 +39,11 @@ module tb_part2();
 		   $warning("output data overflow happened while accumulating squared inputs with data being %x and %x", out_data_old, out_data_new);
 
    end // initial begin overflow test
+   */
 
-   initial begin
-	   // call reset checks task
-	   reset_checks();
-   end
+   //initial begin
+	// call reset checks task
+	//reset_checks();
+   //end
 
 endmodule // tb_part2_mac
