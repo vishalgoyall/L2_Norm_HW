64726a4728cda18064ed5230ebd25270690b66c0
Prateek <prate93@gmail.com>
2019-09-22 16:34:14 -0400

    reset_checks.sv

--- part2/tasks/reset_checks.sv
+++ part2/tasks/reset_checks.sv
@@ -1,10 +1,10 @@
-//Following Tests would be done to check thoroughness of reset along with other input control params like clock and valid_en
+//Following Test is done to check thoroughness of reset along with other input control params like clock and valid_en
 
 task reset_checks;
-
- //TEST 1	 
+$display("\n//++++++++++++++++++++++++++++++++++++++\n// Starting Reset Checks\n//++++++++++++++++++++++++++++++++++++++");
+ //TEST 	 
  //Back to Back reset check : Reset to be checked for all possible values of Valid IN along with reset signal
- //Sequence will be Valid Data (Valid EN = 1) -> Reset (Valid EN = 1) -> Valid data (Valid EN = 1) -> Reset (Valid EN = 0) 
+ //Sequence will be  Valid EN = 1 -> Reset ->  Valid EN = 1 -> Reset & Valid EN = 0 -> Valid En = 0 -> Reset & Valid En = 1 -> Valid EN = 1  
  //Design asserts will fail if reset is not working correctly
  @(posedge clk)
  #1
@@ -28,7 +28,20 @@ task reset_checks;
  @(posedge clk)
  #1
  reset = 1'b0;
-
+ @(posedge clk)
+ #1
+ reset = 1'b1;
+ valid_in = 1'b1;
+ a = 8'd4;
+ @(posedge clk)
+ #1
+ reset = 1'b0;
+ valid_in = 1'b1;
+ @(posedge clk)
+ #1
+ valid_in = 1'b1;
+ a = 8'd10;
+$display("\n//+++++++ DONE +++++++++++++++++++++++++\n");
 
 endtask
 
