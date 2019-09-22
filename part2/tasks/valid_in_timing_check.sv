75d7200af90e36e6cfa04480b3aad574c30bc004
Prateek <prate93@gmail.com>
2019-09-22 18:49:28 -0400

    updated with corrected syntax and display

--- part2/tasks/valid_in_timing_check.sv
+++ part2/tasks/valid_in_timing_check.sv
@@ -2,14 +2,22 @@
 //Creating a random number and trying to delay assertion and deassertion w.r.t. clock edge
 //Design assertions will check for any issue
 
-task valid_in_timing_checks();
-       for (int i = 0; i < 99; i++) begin
-	       @(posedge clk) begin  //assertion randomization
-		       #($urandom_range(95,5)*100ps) valid_in = 1'b1;
-		       valid_data = $urandom_range(256,1);
-	       end
-	       @(posedge clk) begin //deassertion randomization
-		       #($urandom_range(95,5)*100ps) valid_in = 1'b0;
-	       end
-       end 
+task valid_in_timing_check();
+ int assert_dly;
+ int deassert_dly;
+ $display("\n//++++++++++++++++++++++++++++++++++++++\n// Starting Valid In Timing Check\n//++++++++++++++++++++++++++++++++++++++");
+ $display("Current TimeStamp is %2d ns",$realtime);
+        for (int i = 0; i < 99; i++) begin
+ 	       assert_dly   = $urandom_range(9,1);
+ 	       deassert_dly = $urandom_range(9,1);
+ 	       $display("assert valid in = %2d ns after pos edge, deassert valid in = %2d ns after next posedge", assert_dly, deassert_dly);
+ 	       @(posedge clk) begin  //assertion randomization
+ 		       # assert_dly valid_in = 1'b1;
+ 		       # 10 a = $urandom_range(256,1);
+ 	       end
+ 	       @(posedge clk) begin //deassertion randomization
+ 		       # deassert_dly valid_in = 1'b0;
+ 	       end
+        end 
+ $display("\n//+++++++ DONE +++++++++++++++++++++++++\n");
 endtask
