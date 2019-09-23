8c57e17fa81e20426e22ee8f54993426b83888f4
Prateek <prate93@gmail.com>
2019-09-22 20:42:59 -0400

    added files and updated tb for part3

--- part3/part3_tb.sv
+++ part3/part3_tb.sv
@@ -1,33 +1,51 @@
-// This is a very small testbench for you to check that you have the right
-// idea for the input/output timing.
+// Project 1 : Part 2 testbench
+// Authors : Prateek Jain and Vishal Goyal
 
-// 
-// 
-
-module tb_part3();
+module tb_part2();
 
    logic clk, reset, valid_in, valid_out, overflow;
    logic [7:0] a;
-   logic [9:0] g;
+   logic [19:0] f;
 
-   part3 dut(.clk(clk), .reset(reset), .a(a), .valid_in(valid_in), .g(g), .valid_out(valid_out));
+   part2 dut(.clk(clk), .reset(reset), .a(a), .valid_in(valid_in), .f(f), .valid_out(valid_out));
 
    initial clk = 0;
    always #5 clk = ~clk;
 
    // PLACE HOLDER FOR ALL TASK FILES
    `include "./tasks/reset_checks.sv"
-   `include "./tasks/arith_checks.sv"
    `include "./tasks/valid_toggle_checks.sv"
+   `include "./tasks/valid_in_timing_check.sv"
+   `include "./tasks/invalid_data_check.sv"
    `include "./tasks/overflow_check.sv"
 
    initial begin
-	//arith_checks(); // Commenting this check out as this is already a part of the next one
-	valid_toggle_checks();
+        // Before first clock edge, initialize
+        reset = 1;
+        a = 0;
+        valid_in = 0;
+
+        //Remove Reset 
+        @(posedge clk);
+        #1; 
+        reset = 0; 
+
+        //Call Reset Checks task 
 	reset_checks();
+
+	//Call task to check for arithmetic sanity with valid and non valid data streams
+	valid_toggle_checks();
+
+	//Call task for sweeping valid in for different timings
+        valid_in_timing_check();
+
+	//Call task where only data stream is toggled but valid in is kept 0
+        invalid_data_check();
+
+	//Call task to cause overflow
         overflow_check();
 	#20;
 	$finish;
    end
 
-endmodule // tb_part3_mac
+endmodule // tb_part2_mac
---
+++ part3/tasks/valid_in_timing_check.sv
@@ -0,0 +1,29 @@
+//Task to sweep through different timings on valid_en assertion and deassertion
+//Creating a random number and trying to delay assertion and deassertion w.r.t. clock edge
+//Design assertions will check for any issue
+
+task valid_in_timing_check();
+ int assert_dly;
+ int deassert_dly;
+ int i;
+ $display("\n//++++++++++++++++++++++++++++++++++++++\n// Starting Valid In Timing Check\n//++++++++++++++++++++++++++++++++++++++");
+ $display("Current TimeStamp is %2d ns",$realtime);
+        @(posedge clk) 
+                #1 reset = 1'b1;
+        @(posedge clk) 
+                #1 reset = 1'b0;
+
+        for (i = 0; i < 99; i++) begin
+ 	       assert_dly   = $urandom_range(9,1);
+ 	       deassert_dly = $urandom_range(9,1);
+ 	       $display("assert valid in = %2d ns after pos edge, deassert valid in = %2d ns after next posedge", assert_dly, deassert_dly);
+ 	       @(posedge clk) begin  //assertion randomization
+ 		       # assert_dly valid_in = 1'b1;
+ 		       a = $urandom_range(256,1);
+ 	       end
+ 	       @(posedge clk) begin //deassertion randomization
+ 		       # deassert_dly valid_in = 1'b0;
+ 	       end
+        end 
+ $display("\n//+++++++ DONE +++++++++++++++++++++++++\n");
+endtask
