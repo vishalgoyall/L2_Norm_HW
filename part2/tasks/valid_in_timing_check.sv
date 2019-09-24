12ac17285fd6c1d332d0e4441599fd34e0045d5c
Prateek <prate93@gmail.com>
2019-09-23 20:18:13 -0400

    made test more robust by adding valid_in deassetion in same clock as well

--- part2/tasks/valid_in_timing_check.sv
+++ part2/tasks/valid_in_timing_check.sv
@@ -5,7 +5,9 @@
 task valid_in_timing_check();
  int assert_dly;
  int deassert_dly;
+ int rand_sel;
  int i;
+ parameter timing_check_count = 1000;
  $display("\n//++++++++++++++++++++++++++++++++++++++\n// Starting Valid In Timing Check\n//++++++++++++++++++++++++++++++++++++++");
  $display("Current TimeStamp is %2d ns",$realtime);
         @(posedge clk) 
@@ -13,17 +15,30 @@ task valid_in_timing_check();
         @(posedge clk) 
                 #1 reset = 1'b0;
 
-        for (i = 0; i < 99; i++) begin
+        for (i = 0; i < timing_check_count; i++) begin
  	       assert_dly   = $urandom_range(9,1);
  	       deassert_dly = $urandom_range(9,1);
- 	       $display("assert valid in = %2d ns after pos edge, deassert valid in = %2d ns after next posedge", assert_dly, deassert_dly);
+	       rand_sel     = $urandom_range(1,0);
+ 	       //$display("assert valid in = %2d ns after pos edge, deassert valid in = %2d ns after next posedge", assert_dly, deassert_dly);
  	       @(posedge clk) begin  //assertion randomization
  		       # assert_dly valid_in = 1'b1;
  		       a = $urandom_range(256,1);
  	       end
- 	       @(posedge clk) begin //deassertion randomization
+	       if (rand_sel == 0) begin                     //you can deassert in same cycle or next cycle based on assertion dly
+		       if (assert_dly >= 1 && assert_dly <= 8) begin   //deassert in same cycle as assertion 
+                           deassert_dly = $urandom_range(9-assert_dly,1); 
+ 		           # deassert_dly valid_in = 1'b0;
+			   end 
+		       else begin                                    //or deassert in next cycle
+	        	       @(posedge clk) 
+ 		               # deassert_dly valid_in = 1'b0;
+	               end
+	       end 
+	       else begin                                    //or deassert in next cycle
+		       @(posedge clk) begin 
  		       # deassert_dly valid_in = 1'b0;
- 	       end
+	               end
+               end
         end 
  $display("\n//+++++++ DONE +++++++++++++++++++++++++\n");
 endtask
